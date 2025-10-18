705620134711512b9797e45c9ae5b1d63105ee32
Rishab Angadi <rishab.gfb@gmail.com>
2025-10-18 20:04:28 +0530

    added trpc support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-"use client";
-import { prisma } from "@/lib/prisma";
-
-const Page = async () => {
-  const users = await prisma.user.findMany();
-
+const Page = async () => { 
   return (
     <div className="min-h-screen w-screen flex items-center justify-center">
-      {JSON.stringify(users)}
+      {/* {JSON.stringify(users)} */}
     </div>
   );
 };
